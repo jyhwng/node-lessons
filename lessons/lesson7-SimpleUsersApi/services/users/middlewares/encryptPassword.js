ba56c868c3bf526b08f639d020c5560ed363e590
jyhwng <jy.hwng@gmail.com>
2020-04-13 22:30:29 +0200

    Create encrypt password middleware

--- lessons/lesson7-SimpleUsersApi/services/users/middlewares/encryptPassword.js
+++ lessons/lesson7-SimpleUsersApi/services/users/middlewares/encryptPassword.js
@@ -1,25 +1,18 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   let data = req.body;
   let errors = [];
 
-  // Check whether there is the `password` property
   if (!data.password) {
     errors.push('The password is missing.');
   } else {
-    // Encrypt the password
-    data.password = bcrypt.hashSync(data.password, saltRounds);
+    data.password = await bcrypt.hash(data.password, saltRounds);
   }
-
-  // For the sake of simplicity we omit a 'passwordConfirmation' field,
-  // but it is a good practice to always use it
 
   if (errors.length > 0) {
-    res.status(422).json({ hasError: 1, error: errors.toString() });
-    return;
+    return res.status(422).json({ hasError: 1, error: errors.toString() });
   }
-
   next();
 };
