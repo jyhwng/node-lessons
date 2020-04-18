3e052052ceabc19cdf15a0feeb21ea0dd534583e
jyhwng <jy.hwng@gmail.com>
2020-04-18 21:38:17 +0200

    Create login.validation middleware

--- lessons/lesson8-Authentication/services/sessions/middlewares/login.validation.js
+++ lessons/lesson8-Authentication/services/sessions/middlewares/login.validation.js
@@ -1,60 +1,35 @@
-const bcrypt = require('bcryptjs');
-const saltRounds = 10;
+const bcrypt = require('bcrypt');
 
 module.exports = async (req, res, next) => {
-  const UsersModel = req.app.get('usersModel');
-
-  let data = req.body;
-  let errors = [];
+  const { email, password } = req.body;
+  const UserModel = req.app.get('usersModel');
 
   if (!req.context) {
     req.context = {};
   }
 
-  if (!data.email || !data.password) {
-    if (!data.email) {
-      errors.push('The Email is missing.');
-    }
-
-    if (!data.password) {
-      errors.push('The Password is missing.');
-    }
-  } else {
-    // Check whether the User exists
-
-    const user = await UsersModel.findOne({
-      email: data.email
-    });
-
-    if (!user) {
-      // Hash random string to protect against timing attacks
-      bcrypt.hashSync('random', saltRounds);
-
-      res.status(403).json({ hasError: 1, error: 'Wrong credentials.' });
-      return;
-    }
-
-    // Verify the Password
-
-    if (await !bcrypt.compare(data.password, user.password)) {
-      res.status(403).json({ hasError: 1, error: 'Wrong credentials.' });
-      return;
-    }
-
-    // Save the User id and data in the context object to make it available after the middleware ran
-    req.context.id = user._id;
-    req.context.user = {
-      id: user._id,
-      email: user.email,
-      username: user.username,
-      admin: user.admin || false
-    };
+  if (!email || !password) {
+    return res.status(422).json({ hasError: 1, error: 'Email or password is not given' });
   }
 
-  if (errors.length > 0) {
-    res.status(422).json({ hasError: 1, error: errors.join(' ') });
-    return;
+  const user = await UserModel.findOne({ email });
+
+  if (!user) {
+    return res.status(401).json({ hasError: 1, error: 'User Not Found' });
   }
+
+  const verified = await bcrypt.compare(password, user.password);
+
+  if (!verified) {
+    return res.status(401).json({ hasError: 1, error: 'Login failed' });
+  }
+
+  req.context.user = {
+    id: user._id,
+    email: user.email,
+    username: user.username,
+    admin: user.admin || false,
+  };
 
   next();
 };
