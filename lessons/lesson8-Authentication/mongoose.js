bb38f4cb1fa88707bb6e38d83236c2a8b06f99c5
jyhwng <jy.hwng@gmail.com>
2020-04-18 22:04:03 +0200

    Update mongoose file

--- lessons/lesson8-Authentication/mongoose.js
+++ lessons/lesson8-Authentication/mongoose.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const nconf = require('nconf');
 
-mongoose.connect(
-  `${nconf.get('db_adapter')}://${nconf.get('db_username')}:${nconf.get('db_password')}@${nconf.get('db_host')}:${nconf.get('db_port')}/${nconf.get('db_name')}`, {})
-  .catch(e => {
+mongoose
+  .connect(
+    `${nconf.get('db_adapter')}://${nconf.get('db_username')}:${nconf.get('db_password')}@${nconf.get(
+      'db_host'
+    )}/${nconf.get('db_name')}?retryWrites=true&w=majority`,
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+  .catch((e) => {
     console.log(e);
   });
-// mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
