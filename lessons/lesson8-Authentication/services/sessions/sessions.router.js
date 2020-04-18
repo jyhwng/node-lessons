b5ea6241bf97a9eb78a2401fb036e93090fadf96
jyhwng <jy.hwng@gmail.com>
2020-04-18 21:49:10 +0200

    Create sessions.router

--- lessons/lesson8-Authentication/services/sessions/sessions.router.js
+++ lessons/lesson8-Authentication/services/sessions/sessions.router.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const loginValidation = require('./middlewares/login.validation');
 
 module.exports = (app) => {
-  // Check user credentials
-
   router.post('/', loginValidation);
 
-  /**
-   * Authenticate the User by creating a new token.
-   */
   router.post('/', (req, res) => {
     const jwtManager = app.get('jwtManager');
-    const token = jwtManager.createToken(req.context.id);
-
+    const token = jwtManager.createToken(req.context.user.id);
     res.set('Authorization', `Bearer ${token}`);
-    res.json({ data: req.context.user });
+    return res.json({ data: req.context.user });
   });
 
   return router;
